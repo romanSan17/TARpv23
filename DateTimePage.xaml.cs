62aa1da7babde40fe4e74d72a7b51e71da9e6f16
romanSan17 <153905058+romanSan17@users.noreply.github.com>
2025-03-03 13:15:37 +0200

    Add files via upload

---
+++ DateTimePage.xaml.cs
@@ -0,0 +1,50 @@
+using Microsoft.Maui.Layouts;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TARpv23;
+
+public partial class DateTimePage : ContentPage
+{
+    Label lbl;
+    DatePicker dp;
+    TimePicker tp;
+    AbsoluteLayout abs;
+
+    public DateTimePage(int k)
+    {
+        lbl = new Label();
+        {
+            BackgroundColor = Color.FromRgb(120, 44, 133);
+            Text = "Vali mingi kuupäev või aeg";
+        };
+        dp = new DatePicker
+        {
+            MinimumDate = DateTime.Now.AddDays(-10),
+            MaximumDate = DateTime.Now.AddDays(10),
+            Format = "D"
+        };
+        dp.DateSelected += Kuupaeva_valik;
+        tp = new TimePicker
+        {
+            Time = new TimeSpan(12, 0, 0),
+        };
+        tp.PropertyChanged += Aega_valik;
+        abs = new AbsoluteLayout { Children = { lbl, dp, tp } };
+        AbsoluteLayout.SetLayoutBounds(lbl, new Rect(10, 10, 200, 50));
+        AbsoluteLayout.SetLayoutBounds(dp, new Rect(0.2, 0.2, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+        AbsoluteLayout.SetLayoutFlags(dp, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(tp, new Rect(0.2, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+        AbsoluteLayout.SetLayoutFlags(tp, AbsoluteLayoutFlags.PositionProportional);
+        Content abs;
+    }
+
+    private void Aega_valik(object? sender, System.ComponentModel.PropertChangedEventArgs e)
+    {
+        lbl.Text = "Oli valitud aeg: " + e.NewValue.ToString();
+    }
+
+    private void Kuupaeva_valik(object? sender, DateChangedEventArgs e)
+    {
+        lbl.Text = "Oli valitud kuupäev: " + e.NewDate.ToString("F");
+    }
+}
